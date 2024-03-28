bf57702cf8c658a1cf1a7b14273a9b9ebfd42dff
tomas6446 <tomas.kozakas@gmail.com>
2024-03-28 23:14:00 +0200

    Remove unused script

--- 02/indicator.py
+++ 02/indicator.py
@@ -1,116 +1,116 @@
-import sys
-
+import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import yfinance as yf
+from matplotlib.gridspec import GridSpec
+from sklearn.linear_model import LinearRegression
+import matplotlib as mpl
 
 # Date,Time,Open,High,Low,Close,Volume
-data_file_path = '../data/EURUSD_240.csv'
+data_file_path = '../data/indicator_data.csv'
+ticker = 'EURUSD=X'
+from_date = '1994-01-01'
+to_date = '2024-01-01'
+
+lrs_window = 14
+stos_period = 50
+bop_period = 14
+
+# Set global parameters to increase text and line sizes
+mpl.rcParams['axes.labelsize'] = 14  # For x and y labels
+mpl.rcParams['axes.titlesize'] = 16  # For the title
+mpl.rcParams['xtick.labelsize'] = 12  # For x-axis tick labels
+mpl.rcParams['ytick.labelsize'] = 11  # For y-axis tick labels
+mpl.rcParams['legend.fontsize'] = 11  # For the legend
+mpl.rcParams['lines.linewidth'] = 1   # For the lines in the plot
+mpl.rcParams['figure.figsize'] = [20, 12]
 
 
-def calculate_KVO(data, short_period=34, long_period=55):
-    """
-    Calculate Klinger Volume Oscillator (KVO)
-    """
-    dm = data['High'] - data['Low']
-    cm = dm - dm.shift(1)
-    volume_force = cm * data['Volume']
-    kvo = volume_force.ewm(span=short_period, adjust=False).mean() - volume_force.ewm(span=long_period,
-                                                                                      adjust=False).mean()
-    data['KVO'] = kvo
+# Calculate Linear Regression Slope
+def calculate_LRS(data):
+    data['LR_Slope'] = np.nan
+
+    for i in range(lrs_window, len(data)):
+        x = np.arange(lrs_window).reshape(-1, 1)
+        y = data['Close'].iloc[i - lrs_window:i].values
+        model = LinearRegression().fit(x, y)
+        data.loc[data.index[i], 'LR_Slope'] = model.coef_[0]
+
     return data
 
 
-def calculate_MA(data, period=20):
-    """
-    Calculate Moving Average (MA)
-    """
-    data['MA'] = data['Close'].rolling(window=period).mean()
+# Calculate High-Low (H-L)
+def calculate_HL(data):
+    data['HL'] = data['High'] - data['Low']
     return data
 
 
-def calculate_ATR(data, period=14):
-    """
-    Calculate Average True Range (ATR)
-    """
-    data['high-low'] = data['High'] - data['Low']
-    data['high-prev_close'] = abs(data['High'] - data['Close'].shift(1))
-    data['low-prev_close'] = abs(data['Low'] - data['Close'].shift(1))
-    data['true_range'] = data[['high-low', 'high-prev_close', 'low-prev_close']].max(axis=1)
-    data['ATR'] = data['true_range'].rolling(window=period).mean()
-    return data.drop(columns=['high-low', 'high-prev_close', 'low-prev_close', 'true_range'])
+# Calculate Balance of Power (BOP)
+def calculate_BOP(data):
+    BOP = (data['Close'] - data['Open']) / (data['High'] - data['Low'])
+    data['BOP_raw'] = BOP.fillna(0)  # Fill any NaN results with 0
+    data['BOP'] = data['BOP_raw'].rolling(window=bop_period).mean().fillna(0)  # Smoothed BOP
+    return data
 
 
-def show_graph(data, plot_close=True, plot_ma=False, plot_kvo=False, plot_atr=False):
-    """
-    Show graph with options to plot Close Price, MA, KVO, and ATR.
-    """
-    fig, ax = plt.subplots(figsize=(14, 7))
-    ax2 = ax.twinx()  # Prepare a second y-axis
+def plot_stock_with_indicators(data, plot_LRS=False, plot_HL=False, plot_BOP=False, main_plot_type='line'):
+    subplots_count = 1 + sum([plot_LRS, plot_HL, plot_BOP])
+    height_ratios = [2] + [1] * (subplots_count - 1)
+    fig = plt.figure(figsize=(16, 1 + 3 * subplots_count))
+    gs = GridSpec(subplots_count, 1, height_ratios=height_ratios, hspace=0.05)
 
-    handles1, labels1 = [], []
-    handles2, labels2 = [], []
+    ax_main = fig.add_subplot(gs[0])
+    if main_plot_type == 'column':
+        ax_main.bar(data.index, data['Close'], label='Close Price', color='skyblue', width=0.5)
+    else:  # Default to 'line'
+        ax_main.plot(data.index, data['Close'], label='Close Price', color='skyblue')
 
-    if plot_close:
-        ax.plot(data['Close'], label='Close Price', color='black')
-        ax.set_xlabel('Date')
-        ax.set_ylabel('Price')
-        handles1, labels1 = ax.get_legend_handles_labels()
+    ax_main.set_ylabel('Stock Price')
+    ax_main.legend()
+    ax_main.yaxis.tick_right()
+    ax_main.grid(True, which='both', linestyle='--')
 
-    if plot_ma:
-        ax.plot(data['MA'], label='Moving Average', color='blue')
-        handles1, labels1 = ax.get_legend_handles_labels()
+    current_subplot = 1
+    if plot_LRS:
+        data = calculate_LRS(data)
+        ax_lrs = fig.add_subplot(gs[current_subplot], sharex=ax_main)
+        ax_lrs.plot(data.index, data['LR_Slope'], label='Linear Regression Slope', color='green')
+        ax_lrs.set_ylabel('LRS', fontsize=12)
+        ax_lrs.legend()
+        ax_lrs.yaxis.tick_right()
+        ax_lrs.grid(True, linestyle=':')
+        current_subplot += 1
 
-    if plot_kvo:
-        ax2.plot(data['KVO'], label='KVO', color='red')
-        ax2.axhline(y=0, color='gray', linestyle='--')
-        ax2.set_ylabel('KVO')
-        handles2, labels2 = ax2.get_legend_handles_labels()
+    if plot_HL:
+        data = calculate_HL(data)
+        ax_hl = fig.add_subplot(gs[current_subplot], sharex=ax_main)
+        ax_hl.plot(data.index, data['HL'], label='High-Low', color='red', linewidth=2)
+        ax_hl.set_ylabel('HL', fontsize=12)
+        ax_hl.legend()
+        ax_hl.yaxis.tick_right()
+        ax_hl.grid(True, linestyle=':')
+        current_subplot += 1
 
-    if plot_atr:
-        ax.plot(data['ATR'], label='ATR', color='green')
-        ax.set_ylabel('ATR')
-        handles1, labels1 = ax.get_legend_handles_labels()
+    if plot_BOP:
+        data = calculate_BOP(data)
+        ax_bop = fig.add_subplot(gs[current_subplot], sharex=ax_main)
+        ax_bop.plot(data.index, data['BOP'], label='Balance of Power', color='purple', linewidth=2)
+        ax_bop.set_ylabel('BOP', fontsize=12)
+        ax_bop.legend()
+        ax_bop.yaxis.tick_right()
+        ax_bop.grid(True, linestyle=':')
 
-    # Combine handles and labels from both y-axes
-    handles, labels = handles1 + handles2, labels1 + labels2
-
-    # Create a single legend with entries from both axes
-    fig.legend(handles, labels, loc='upper left')
-    ax.grid(which='major', linestyle='--', linewidth=0.5, color='grey')
-    plt.title('Tesla Stock Analysis')
+    plt.setp(ax_main.get_xticklabels(), visible=False)
+    ax_main.xaxis.set_major_locator(mdates.AutoDateLocator())
+    ax_main.xaxis.set_major_formatter(mdates.DateFormatter("%b %Y"))
+    fig.autofmt_xdate()
+    plt.tight_layout()
     plt.show()
 
 
-def read(file_path, from_date=None, to_date=None):
-    d = pd.read_csv(file_path)
-    d['DateTime'] = pd.to_datetime(d['Date'] + ' ' + d['Time'])
-    d.set_index('DateTime', inplace=True)
-    d.drop(columns=['Date', 'Time'], inplace=True)
+data = yf.download(ticker, start=from_date, end=to_date)
+data.to_csv(data_file_path)
+data = pd.read_csv(data_file_path, index_col='Date', parse_dates=True)
 
-    # Convert from_date and to_date to datetime objects if they are not None
-    if from_date:
-        from_datetime = pd.to_datetime(from_date)
-        d = d[d.index >= from_datetime]
-    if to_date:
-        to_datetime = pd.to_datetime(to_date)
-        d = d[d.index <= to_datetime]
-
-    # Correctly sort the DataFrame in ascending order
-    d = d.sort_index(ascending=True)
-
-    return d
-
-
-if len(sys.argv) > 1:
-    data_file_path = sys.argv[1]
-
-data = read(data_file_path, '2018-01-01', '2024-01-01')
-
-data_ma = calculate_MA(data)
-show_graph(data_ma, plot_close=True, plot_ma=True)
-
-data_kvo = calculate_KVO(data)
-show_graph(data_ma, plot_close=True, plot_kvo=True)
-
-data_atr = calculate_ATR(data)
-show_graph(data_ma, plot_close=True, plot_atr=True)
+plot_stock_with_indicators(data, plot_LRS=True, plot_BOP=True, plot_HL=True, main_plot_type='line')
