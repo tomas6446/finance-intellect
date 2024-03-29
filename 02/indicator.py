dbc885b75e4924b680afbbb7cc6a8d191c095912
tomas6446 <tomas.kozakas@gmail.com>
2024-03-29 12:51:18 +0200

    Correct fig size

--- 02/indicator.py
+++ 02/indicator.py
@@ -6,16 +6,17 @@ import yfinance as yf
 from matplotlib.gridspec import GridSpec
 from sklearn.linear_model import LinearRegression
 import matplotlib as mpl
+from matplotlib.ticker import MultipleLocator
 
 # Date,Time,Open,High,Low,Close,Volume
 data_file_path = '../data/indicator_data.csv'
 ticker = 'EURUSD=X'
-from_date = '1994-01-01'
+from_date = '2004-01-01'
 to_date = '2024-01-01'
 
 lrs_window = 14
 stos_period = 50
-bop_period = 14
+bop_period = 30
 
 # Set global parameters to increase text and line sizes
 mpl.rcParams['axes.labelsize'] = 14  # For x and y labels
@@ -77,9 +78,12 @@ def plot_stock_with_indicators(data, plot_LRS=False, plot_HL=False, plot_BOP=Fal
         data = calculate_LRS(data)
         ax_lrs = fig.add_subplot(gs[current_subplot], sharex=ax_main)
         ax_lrs.plot(data.index, data['LR_Slope'], label='Linear Regression Slope', color='green')
+        ax_lrs.set_ylim(-0.03, 0.02)
+        ax_lrs.yaxis.set_major_locator(MultipleLocator(0.01))
         ax_lrs.set_ylabel('LRS', fontsize=12)
         ax_lrs.legend()
         ax_lrs.yaxis.tick_right()
+
         ax_lrs.grid(True, linestyle=':')
         current_subplot += 1
 
@@ -87,6 +91,8 @@ def plot_stock_with_indicators(data, plot_LRS=False, plot_HL=False, plot_BOP=Fal
         data = calculate_HL(data)
         ax_hl = fig.add_subplot(gs[current_subplot], sharex=ax_main)
         ax_hl.plot(data.index, data['HL'], label='High-Low', color='red', linewidth=2)
+        ax_hl.set_ylim(-0.1, 1.5)
+        ax_hl.yaxis.set_major_locator(MultipleLocator(0.5))
         ax_hl.set_ylabel('HL', fontsize=12)
         ax_hl.legend()
         ax_hl.yaxis.tick_right()
@@ -97,13 +103,15 @@ def plot_stock_with_indicators(data, plot_LRS=False, plot_HL=False, plot_BOP=Fal
         data = calculate_BOP(data)
         ax_bop = fig.add_subplot(gs[current_subplot], sharex=ax_main)
         ax_bop.plot(data.index, data['BOP'], label='Balance of Power', color='purple', linewidth=2)
+        ax_bop.set_ylim(-1, 1)
+        ax_bop.yaxis.set_major_locator(MultipleLocator(0.5))
         ax_bop.set_ylabel('BOP', fontsize=12)
         ax_bop.legend()
         ax_bop.yaxis.tick_right()
         ax_bop.grid(True, linestyle=':')
 
     plt.setp(ax_main.get_xticklabels(), visible=False)
-    ax_main.xaxis.set_major_locator(mdates.AutoDateLocator())
+    ax_main.xaxis.set_major_locator(mdates.YearLocator())
     ax_main.xaxis.set_major_formatter(mdates.DateFormatter("%b %Y"))
     fig.autofmt_xdate()
     plt.show()
